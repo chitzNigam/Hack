00e86ac5b5910891580b7adf443cf3d8c8bf1a39
Kashyap Nasit <kashyapnasit@gmail.com>
2020-02-15 05:51:05 +0530

    Basic changes

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,13 +1,42 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract SimpleStorage {
-  uint storedData;
+  
+  mapping (bytes32 => bytes32) private groips;
+  
+  // function set(uint x) public {
+  //   storedData = x;
+  // }
 
-  function set(uint x) public {
-    storedData = x;
+  // function get() public view returns (uint) {
+  //   return storedData;
+  // }  
+
+  function _getHash (string storage client1, string storage client2) private returns(bytes32) {
+  	string storage long = "";
+  	string storage short = "";
+  	if(client2>client1){
+  		long=client2;
+  		short=client1;
+  	}
+  	else{
+  		long=client1;
+  		short=client2;
+  	}
+
+  	return sha256(concat(long,short));  	
   }
 
-  function get() public view returns (uint) {
-    return storedData;
-  }
+  function concat(bytes memory a, bytes memory b)
+            internal pure returns (bytes memory) {
+        return abi.encodePacked(a, b);
+    }
+
+  	function _toString(address x) private returns(string storage) {
+	    bytes memory b = new bytes(20);
+	    for (uint i = 0; i < 20; i++)
+	        b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+	    return string(b);
+	}
+  
 }
